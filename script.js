5a41b9d41eccfb6240e2b78fb0a850643e9617d0
Kirillsp1982 <kirillsp1982@gmail.com>
2020-03-15 20:56:00 +0300

    feat: add script.js and some functionality

---
+++ script.js
@@ -0,0 +1,59 @@
+const MENU = document.getElementById('menu');
+const GALARY = document.getElementById('galary');
+const TAB = document.getElementById('portfolio__buttons');
+const LEFTPHONE = document.getElementById('left-phone');
+const RIGHTPHONE = document.getElementById('right-phone');
+const SLIDER = document.getElementById('main__slider');
+const LEFTARROW = document.getElementById('controls__left');
+const RIGHARROW = document.getElementById('controls__right');
+const IMAGES = GALARY.querySelectorAll('img');
+let i = 0;
+
+MENU.addEventListener('click', (event) => {
+	MENU.querySelectorAll('li a').forEach(el => el.classList.remove('menu_active'));
+	event.target.classList.add('menu_active');
+});
+
+GALARY.addEventListener('click', (event) => {
+	IMAGES.forEach(el => el.classList.remove('galary_active'));
+	event.target.classList.add('galary_active');
+});
+
+TAB.addEventListener('click', (event) => {
+	TAB.querySelectorAll('button.portfolio__button').forEach(el => el.classList.remove('button_active'));
+	event.target.classList.add('button_active');
+	GALARY.removeChild(IMAGES[i]);
+	GALARY.appendChild(IMAGES[i]);
+	i++;
+	if (i > 11) i = 0;
+});
+
+LEFTPHONE.addEventListener('click', (event) => {
+	const IMAGE = LEFTPHONE.querySelectorAll('img');
+	IMAGE[0].classList.toggle("hidden");
+	IMAGE[1].classList.toggle('hidden');
+});
+
+RIGHTPHONE.addEventListener('click', (event) => {
+	const IMAGE = RIGHTPHONE.querySelectorAll('img');
+	IMAGE[0].classList.toggle("hidden");
+	IMAGE[1].classList.toggle('hidden');
+});
+ 
+
+
+
+
+
+
+LEFTARROW.addEventListener('click', (event) => {
+	const IMAGE = SLIDER.querySelectorAll('.slide');
+	IMAGE[0].classList.toggle("hidden");
+	IMAGE[1].classList.toggle('hidden');
+});
+
+RIGHARROW.addEventListener('click', (event) => {
+	const IMAGE = SLIDER.querySelectorAll('.slide');
+	IMAGE[0].classList.toggle("hidden");
+	IMAGE[1].classList.toggle('hidden');
+});
